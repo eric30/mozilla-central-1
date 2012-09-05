066691d16cd947dc6729fcb34b5b9ecabd91a34d
Kyle Machulis <kyle@nonpolynomial.com>
2012-09-05 12:57:59 -0700

    IPC Socket Reading Stuff

--- ipc/socket/Socket.h
+++ ipc/socket/Socket.h
@@ -7,8 +7,55 @@
 #ifndef mozilla_ipc_Socket_h
 #define mozilla_ipc_Socket_h
 
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include "mozilla/RefPtr.h"
+
 namespace mozilla {
 namespace ipc {
+
+struct SocketRawData
+{
+  static const size_t MAX_DATA_SIZE = 1024;
+  uint8_t mData[MAX_DATA_SIZE];
+
+  // Number of octets in mData.
+  size_t mSize;
+  size_t mCurrentWriteOffset;
+
+  SocketRawData() :
+    mCurrentWriteOffset(0),
+    mSize(0)
+  {
+  }
+  
+  SocketRawData(const char* str) :
+    mCurrentWriteOffset(0)
+  {
+    memcpy(mData, str, strlen(str));
+    mSize = strlen(str);   
+  }
+};
+
+class SocketConsumer : public RefCounted<SocketConsumer>
+{
+public:
+  SocketConsumer()
+  {}
+  virtual ~SocketConsumer() {}
+  virtual void ReceiveSocketData(SocketRawData* aMessage) = 0;
+  void SendSocketData(SocketRawData* aMessage);
+  void SetFd(int aFd) { mFd = aFd; }
+private:
+  int mFd;
+};
+
+void
+AddSocketWatcher(SocketConsumer* s, int fd);
+
+void
+RemoveSocketWatcher(SocketConsumer* s, int fd);
 
 int
 GetNewSocket(int type, const char* aAddress, int channel, bool auth, bool encrypt);
